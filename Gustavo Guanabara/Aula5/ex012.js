6663cfa991f3886218955d1b5d1e5c95957af9ba
Guilherme <gfernandoportelaoliveira@gmail.com>
2025-12-25 16:49:42 -0300

    Conteudo (condições.pt1)

---
+++ Gustavo Guanabara/Aula5/ex012.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+
+if (vel > 60) { // Condição simples.
+    console.log("Ultrapassou o limite de velocidade, multado!")
+}
+
+console.log('Dirija usando sinto de segurança')
---
+++ Gustavo Guanabara/Aula5/ex013.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}!.`)
+
+if (pais == 'Brasil') { // Condição composta
+    console.log("Brasileiro(a)!.")
+} else {
+    console.log("Estrangeiro(a)!.")
+}
