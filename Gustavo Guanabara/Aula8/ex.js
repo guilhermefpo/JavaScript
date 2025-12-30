1a75ed8dbf1d9c3950355a01ab5ffa328aba00a8
Guilherme <gfernandoportelaoliveira@gmail.com>
2025-12-30 19:44:17 -0300

    Adicionando extrutura de repetição while e do while

---
+++ Gustavo Guanabara/Aula8/ex.js
@@ -0,0 +1,21 @@
+var c = 0
+do {
+    console.log('Olá Tudo Bem?')
+} while (c <= 6)
+
+
+/*var c = 0
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c ++
+}*/
+
+
+
+
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')*/
