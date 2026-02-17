3b1769a60a5ff3d98c428149fda7781ac997491b
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-02-17 13:10:52 -0300

    Add atividades Objetos.js e functions

--- Exercicio_5/script.js
+++ Exercicio_5/script.js
@@ -1,29 +1,44 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.getElementById('res')
-let valores = []
+let num = document.querySelector("input#fnum");
+let lista = document.querySelector("select#flista");
+let res = document.getElementById("res");
+let valores = [];
 
 function isnumero(n) {
-   if (Number(n) >= 1 && Number(n) <= 100) {
-    return true
-   } else {
-    return false
-   }
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function inLista(n, l) {
-   if (l.indexOf(Number(n)) != -1) {
-    return true
-   } else {
-    return false
-   }
+  if (l.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
 function adicionar() {
-    if (isnumero(num.value) && !inLista(num.value, valores)) {
-          alert('Ok!')
-    } else {
-        window.alert('Valor invalido ou já encontrado na lista!')
-    }
+  if (isnumero(num.value) && !inLista(num.value, valores)) {
+    valores.push(Number(num.value));
+    let item = document.createElement("option");
+    item.text = `Valor ${num.value} adicionado.`;
+    lista.appendChild(item);
+    res.innerHTML = "";
+  } else {
+    window.alert("Valor invalido ou já encontrado na lista!");
+  }
+  num.value = "";
+  num.focus();
+}
+
+function finalizar() {
+  if (valores.length === 0) {
+    window.alert("Adicione valores antes de finalizar!");
+  } else {
+    let tot = valores.length;
+    res.innerHTML = "";
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+  }
 }
