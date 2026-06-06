869ed6888bc3330133847e5104a4fd67fd464ffa
Guilherme Fernando Portela de Oliveira <gfernandoportelaoliveira@gmail.com>
2026-06-06 16:35:47 -0300

    feat: Guanabara

---
+++ Exercicios Guanabara/10_objeto.js
@@ -0,0 +1,16 @@
+// Arrey
+let num = [5, 8, 4];
+
+// Objeto
+let amigo = {
+  nome: "José",
+  sexo: "M",
+  peso: 85.4,
+  engordar(p = 0) {
+    console.log("Engordou");
+    this.peso += p;
+  },
+};
+
+amigo.engordar(2);
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`);
