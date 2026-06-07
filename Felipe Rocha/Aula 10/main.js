2cd8ca45eacc415f7fbdb62d35eb85b90ee2f7cc
Guilherme Fernando Portela de Oliveira <gfernandoportelaoliveira@gmail.com>
2026-06-06 21:48:48 -0300

    Alteração aula 10 Felipe Rocha

--- Felipe Rocha/Aula 10/main.js
+++ Felipe Rocha/Aula 10/main.js
@@ -1,4 +1,28 @@
-// const addUserText = document.getElementById('add-user');
+// Selecionar um elemento:
+// const adicionarUser = window.document.getElementBy("adicionar-user");
+// const adicionarUser = window.document.querySelector(".container #my-form");
 
-// const addUserText = document.querySelector('#add-user')
+// Selecionar multiplos elementos:
+// const varios = document.querySelectorAll(".item")
+// const all = document.getElementsByClassName("item")
+// const all = document.getElementByTagName("li")
 
+// Manipulando elementos:
+// const items = document.querySelector(".items");
+// const button = document.querySelector(".btn");
+// items.remove();
+// items.firstElementChild.remove();
+// items.lastElementChild.remove();
+// items.children[1].textContent = "item Um";
+// items.lastElementChild.innerHTML = "<h1> Hello </h1>";
+// button.style.background = "red";
+// button.style.color = "blue";
+
+// Eventos:
+const button = document.querySelector(".btn");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+});
